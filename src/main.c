faf89ee384571914d6a9812cadd839e0316e4dd3
M B <85039141+m-bartlett@users.noreply.github.com>
2023-07-25 17:25:58 -0600

    simplify main.c

--- src/main.c
+++ src/main.c
@@ -3,23 +3,13 @@
 
 #define _GNU_SOURCE
 
-// #include <sys/time.h>
-// #include <unistd.h>
-// #include <math.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <vitasdk.h>
-
-
 #include "engine/core.h"
 #include "graphics/core.h"
 
-
 int main(int argc, char *argv[]) {
-    engine_init();    
+    engine_init();
     graphics_init();
-    graphics_draw_game();
-    
+
     engine_game_loop();
 
     graphics_end();
