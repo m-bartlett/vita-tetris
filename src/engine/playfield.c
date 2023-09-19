5580f11ae7a75d6145b205501ae447e637a3e2d7
M B <85039141+m-bartlett@users.noreply.github.com>
2023-09-19 15:33:32 -0600

    fix edge-case bugs for piece placement near top

--- src/engine/playfield.c
+++ src/engine/playfield.c
@@ -2,38 +2,26 @@
 #include <string.h>  // strlen
 #include "playfield.h"
 
-// static const uint8_t X_MAX = PLAYFIELD_WIDTH+2;
 static int8_t PLAYFIELD[PLAYFIELD_HEIGHT+4][PLAYFIELD_WIDTH]={{0}};
 
 
 playfield_view_t playfield_view(void) { return (playfield_view_t)PLAYFIELD; }
 
-
-// int8_t playfield_get_block_at_coordinate(uint8_t X, uint8_t Y)
-// { //{{{
-//     return PLAYFIELD[Y][X];
-// /*}}}*/ }
-
-
 uint16_t playfield_get_4x4_vacancy_at_coordinate(uint8_t X, uint8_t Y)
 {/*{{{*/
-    const uint8_t X1=X+1, Y1=Y+1;
-    uint16_t grid = (uint16_t)0x0000;
+    uint16_t grid = (uint16_t)0;
     uint8_t mask_bit = 15;
     
-    for (int8_t y=Y-3; y < Y1; ++y) {
-        if (y<0) {
-            mask_bit -= 4;
-            continue;
-        }
-        if (y > PLAYFIELD_HEIGHT_1) {  // rows above and below the playfield are occupied
+    for (int8_t y=Y-3; y <= Y; ++y) {
+        // if (y > PLAYFIELD_HEIGHT-1) {  // rows above and below the playfield are occupied
+        if (y > PLAYFIELD_HEIGHT-1 || y < 0) {  // rows above and below the playfield are occupied
             mask_bit -= 3;
             grid |= 0b1111 << mask_bit;
             mask_bit--;
             continue;
         }
-        for (int8_t x=X-3; x < X1; ++x) {
-            if (x < 0 || x > PLAYFIELD_WIDTH_1 /* left and right of playfield are occupied */ 
+        for (int8_t x=X-3; x <= X; ++x) {
+            if (x < 0 || x > (PLAYFIELD_WIDTH-1) /* left and right of playfield are occupied */ 
                 || PLAYFIELD[y][x] > 0) {      /* nonzero indices are occupied */
                 grid |= 1 << mask_bit;
             }
@@ -55,20 +43,17 @@ bool playfield_validate_tetromino_placement(tetromino_t* t, uint8_t X, uint8_t Y
 void playfield_place_tetromino(tetromino_t* t, uint8_t X, uint8_t Y)
 { //{{{
     const tetromino_type_t block_type = t->type;
-    const uint8_t X1=X+1, Y1=Y+1;
     const uint16_t grid = tetromino_get_grid(t);
     uint16_t maskbit = (uint16_t)1<<15;
     
-    for (int8_t y=Y-3; y < Y1; ++y) {
-        if (y < 0 || y > PLAYFIELD_HEIGHT) {  // can't modify outside payfield bounds
+    for (int8_t y=Y-3; y <= Y; ++y) {
+        if (y < 0 || y >= PLAYFIELD_HEIGHT) {  // can't modify outside payfield bounds
             maskbit>>=4;
             continue;
         }
-        for (int8_t x=X-3; x < X1; ++x) {
-            if ( !(x < 0 || x > PLAYFIELD_WIDTH) && (maskbit&grid) ) {
-                PLAYFIELD[y][x] = (int8_t)block_type;
-            }
-            maskbit >>=1 ;
+        for (int8_t x=X-3; x <= X; ++x, maskbit >>=1) {
+            if (x < 0 || x >= PLAYFIELD_WIDTH) continue;
+            if (maskbit&grid) PLAYFIELD[y][x] = (int8_t)block_type;
         }
     }
 /*}}}*/ }
@@ -89,6 +74,7 @@ void playfield_clear_line(uint8_t Y)
 /*}}}*/ }
 
 
+
 uint8_t playfield_clear_lines(void (*callback)(uint8_t))
 { //{{{
     uint8_t lines = 0;
@@ -98,8 +84,8 @@ uint8_t playfield_clear_lines(void (*callback)(uint8_t))
             row_occupied = row_occupied && PLAYFIELD[y][x];
         }
         if (row_occupied) {
-            playfield_clear_line(y);
             if (callback != NULL) callback(y);
+            playfield_clear_line(y);
             ++lines;
         }
     }
